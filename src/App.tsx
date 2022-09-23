81e71120d21df97125a6a182046abb1575a30933
Will Hawker <will.hawker@boardintelligence.com>
2022-09-23 10:44:47 +0100

    chore: update starter kit to remirror v2

--- src/App.tsx
+++ src/App.tsx
@@ -5,109 +5,21 @@ import {
   UnderlineExtension
 } from "remirror/extensions";
 import {
+  BasicFormattingButtonGroup,
+  HeadingLevelButtonGroup,
+  HistoryButtonGroup,
   useRemirror,
-  ComponentItem,
   Remirror,
   ThemeProvider,
   Toolbar
 } from "@remirror/react";
 import { AllStyledComponent } from "@remirror/styles/emotion";
-import type { ToolbarItemUnion } from "@remirror/react";
 
 const extensions = () => [
   new HeadingExtension(),
   new BoldExtension({}),
   new ItalicExtension(),
   new UnderlineExtension()
-];
-
-const toolbarItems: ToolbarItemUnion[] = [
-  {
-    type: ComponentItem.ToolbarGroup,
-    label: "History",
-    items: [
-      { type: ComponentItem.ToolbarCommandButton, commandName: "undo", display: "icon" },
-      { type: ComponentItem.ToolbarCommandButton, commandName: "redo", display: "icon" },
-    ],
-    separator: "end",
-  },
-  {
-    type: ComponentItem.ToolbarGroup,
-    label: "Simple Formatting",
-    items: [
-      { type: ComponentItem.ToolbarCommandButton, commandName: "toggleBold", display: "icon" },
-      { type: ComponentItem.ToolbarCommandButton, commandName: "toggleItalic", display: "icon" },
-      { type: ComponentItem.ToolbarCommandButton, commandName: "toggleUnderline", display: "icon" },
-    ],
-    separator: "end",
-  },
-  {
-    type: ComponentItem.ToolbarGroup,
-    label: "Heading Formatting",
-    items: [
-      {
-        type: ComponentItem.ToolbarCommandButton,
-        commandName: "toggleHeading",
-        display: "icon",
-        attrs: { level: 1 },
-      },
-      {
-        type: ComponentItem.ToolbarCommandButton,
-        commandName: "toggleHeading",
-        display: "icon",
-        attrs: { level: 2 },
-      },
-      {
-        type: ComponentItem.ToolbarCommandButton,
-        commandName: "toggleHeading",
-        display: "icon",
-        attrs: { level: 3 },
-      },
-    ],
-    separator: "none",
-  },
-  {
-    type: ComponentItem.ToolbarMenu,
-    label: "Headings",
-    items: [
-      {
-        type: ComponentItem.MenuGroup,
-        role: "radio",
-        items: [
-          {
-            type: ComponentItem.MenuCommandPane,
-            commandName: "toggleHeading",
-            attrs: { level: 1 },
-          },
-          {
-            type: ComponentItem.MenuCommandPane,
-            commandName: "toggleHeading",
-            attrs: { level: 2 },
-          },
-          {
-            type: ComponentItem.MenuCommandPane,
-            commandName: "toggleHeading",
-            attrs: { level: 3 },
-          },
-          {
-            type: ComponentItem.MenuCommandPane,
-            commandName: "toggleHeading",
-            attrs: { level: 4 },
-          },
-          {
-            type: ComponentItem.MenuCommandPane,
-            commandName: "toggleHeading",
-            attrs: { level: 5 },
-          },
-          {
-            type: ComponentItem.MenuCommandPane,
-            commandName: "toggleHeading",
-            attrs: { level: 6 },
-          },
-        ],
-      },
-    ],
-  },
 ];
 
 export default function App() {
@@ -121,7 +33,11 @@ export default function App() {
     <AllStyledComponent>
       <ThemeProvider>
         <Remirror manager={manager} initialContent={state} autoFocus autoRender="end">
-          <Toolbar items={toolbarItems} refocusEditor label="Top Toolbar" />
+          <Toolbar>
+            <HistoryButtonGroup />
+            <BasicFormattingButtonGroup />
+            <HeadingLevelButtonGroup showAll />
+          </Toolbar>
         </Remirror>
       </ThemeProvider>
     </AllStyledComponent>
